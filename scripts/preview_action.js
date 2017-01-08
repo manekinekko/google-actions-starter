fc7a8f9b00c015e04b3e9acbc867a0b5dbcc5fb9
Wassim CHEGHAM <github@wassimchegham.com>
2017-01-08 02:51:12 +0100

    doc: add a comment to preview_action.js

--- scripts/preview_action.js
+++ scripts/preview_action.js
@@ -2,7 +2,7 @@ const exec = require('child_process').execSync;
 const config = require('./update_action_config');
 
 // config.updateConfig().then(actionConfig => {
-
+//     // here you have access to the update to date action config (with the right ngrok URL)
 // });
 
 exec(`gactions --verbose preview --action_package action.json --invocation_name 'my first action' --preview_mins 1234`, { stdio: [0, 1, 2] });
